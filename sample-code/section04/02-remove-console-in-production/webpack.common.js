a2956418509352d9255344851b2595e18f2ad55d
saki matsushita <saki.matsushita@digitalidentity.co.jp>
2020-10-30 20:57:50 +0900

    section3まで

--- sample-code/section04/02-remove-console-in-production/webpack.common.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/js/app.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'js/bundle.js',
-  },
-};
--- sample-code/section08/02-mini-css-extract-plugin/src/js/modules/math.js
+++
@@ -1,3 +0,0 @@
-export const add = (number1, number2) => {
-  return number1 + number2;
-};
